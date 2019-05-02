a1a0443dce5cf75254713c2aec691ceba0df279a
Romain <ferjuleromain@gmail.com>
2019-05-02 13:55:07 +0200

    create footer

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,6 +2,7 @@
   <div>
     <HeaderComponent />
     <HomeCarouselComponent />
+    <FooterComponent/>
   </div>
 </template>
 
@@ -9,12 +10,14 @@
 <script>
 import HeaderComponent from '@/components/common/HeaderComponent';
 import HomeCarouselComponent from '@/components/HomeCarousel';
+import FooterComponent from '@/components/common/FooterComponent';
 
 export default {
   name: 'Home',
   components: {
     HeaderComponent,
     HomeCarouselComponent,
+    FooterComponent,
   },
 };
 </script>
